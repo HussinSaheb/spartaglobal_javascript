c01d78d5269fe658418533bf4610510cd92d2076
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-07 10:55:00 +0100

    renamed vairables for better readable code

--- calc_homework/index.js
+++ calc_homework/index.js
@@ -1,19 +1,20 @@
 // promt the user to type in values
-var a = prompt("please enter number 1");
+var number_1 = prompt("please enter number 1");
 var operator = prompt("plase provide the operator");
-var b = prompt("please enter number 2");
+var number_2 = prompt("please enter number 2");
+
 // convert the values recieved from strings to integers
-a = parseInt(a);
-b = parseInt(b);
+number_1 = parseInt(number_1);
+number_2 = parseInt(number_2);
 
 
-//conditionals for the operators 
+//conditionals for the operators
 if (operator == "+"){
-console.log(a + b);
+console.log(number_1 + number_2);
 }else if (operator == "-") {
-  console.log(a - b);
+  console.log(number_1 - number_2);
 }else if (operator == "*") {
-   console.log(a * b);
+   console.log(number_1 * number_2);
 }else if (operator == "/") {
-   console.log(a / b);
+   console.log(number_1 / number_2);
 };
