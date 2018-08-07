db0a43c12e7da33546e5cd1c155d71c1b2716519
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-07 09:13:19 +0100

    commented the code

--- calc_homework/index.js
+++ calc_homework/index.js
@@ -1,10 +1,13 @@
+// promt the user to type in values
 var a = prompt("please enter number 1");
 var operator = prompt("plase provide the operator");
 var b = prompt("please enter number 2");
-  
+// convert the values recieved from strings to integers
 a = parseInt(a);
 b = parseInt(b);
 
+
+//conditionals for the operators 
 if (operator == "+"){
 console.log(a + b);
 }else if (operator == "-") {
@@ -13,4 +16,4 @@ console.log(a + b);
    console.log(a * b);
 }else if (operator == "/") {
    console.log(a / b);
-}
+};
