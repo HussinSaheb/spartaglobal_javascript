a0e2b5da0154143e923d8c751f276072017a62b9
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-07 21:52:07 +0100

    fixed some code to follow DRY, and cleaned up code

--- advanced_calc_hw/index.js
+++ advanced_calc_hw/index.js
@@ -1,38 +1,56 @@
 
 var chosen = false;
+//loop if the user hasnt chosen a calculator to ask them to chose one
 while(!chosen){
+  //prompt the user for the type of calculator they'd like to use
   var choice = prompt("Please chose the mode you'd like to access\n" + "a - Advanced\n"+ "b - Basic\n" + "i - BMI\n" + "t - Trip\n");
-
-  switch (choice) {
+  // switch on the choice
+  // setting the choice to Lowercase
+  // to make the imput be correct no matter if user
+  // uses uppercase or lowercase letters
+  switch (choice.toLowerCase()) {
+    // if A, is chosen then
     case "a":
        advancedCalc();
-       chosen = true;
     break;
     case "b":
        basicCalc();
-       chosen = true;
     break;
     case "i":
       bmiCalc();
-      chosen = true;
     break;
     case "t":
       tripCalc();
-      chosen = true;
     break;
     default:
-    chosen = false;
+    alert("")
  }
 }
-function prompt_2_numbers() {
-  var first_number = parseFloat(prompt("please enter first number"));
-  var second_number = parseFloat(prompt("please enter second number"));
-  return [first_number, second_number];
+// custom function to crete prompts, with first argument being the amount of prompts to create
+// the msg_1,msg_2,msg_3,msg_4 parameters are there with a default string value for msg1 and msg2
+// this is can be overwritten by passing new arguments when the function is called elsewhere.
+function prompt_values(amount_of_prompts,msg_1= "please enter first number",msg_2 = "please enter second number",msg_3 = "",msg_4="" ) {
+    //create the variables to stope the prompts return value
+  var first_number,second_number,third_number fourth_number;
+  if (amount_of_prompts == 2) {
+     first_number = parseFloat(prompt(msg_1));
+     second_number = parseFloat(prompt(msg_2));
+    // return the value as an array
+    return [first_number, second_number];
+  }else if (amount_of_prompts ==4 ) {
+    // assign the values of the prompt to  msg1-4
+     first_number = parseFloat(prompt(msg_1));
+     second_number = parseFloat(prompt(msg_2));
+     third_number = parseFloat(prompt(msg_3));
+     fourth_number = parseFloat(prompt(msg_4));
+     // return the values as an array
+    return [first_number, second_number,third_number,fourth_number];
+  }
 }
 
 function advancedCalc() {
   var type =  parseInt(prompt("please chose \n" + "1 - Power\n" +" 2- Square Root\n" ));
-  var array = prompt_2_numbers();
+  var array = prompt_values(2);
   number_1 = array[0];
   number_2 = array[1];
   if (type==1) {
@@ -43,54 +61,77 @@ function advancedCalc() {
 }
 
 function basicCalc(){
-  var type =  parseInt(prompt("please chose your operation \n" + "1 - Addition (+)\n" +" 2 - Subtraction (-)\n"+" 3 - Mulitplication (*) Root\n"+" 4 - Division (/)\n" ));
-  var array = prompt_2_numbers();
+  //prompt to chose what operator to use
+  var operator =  parseInt(prompt("please chose your operation \n" + "1 - Addition (+)\n" +" 2 - Subtraction (-)\n"+" 3 - Mulitplication (*) Root\n"+" 4 - Division (/)\n" ));
+  // use our function with 2 prompts, and default messages for 2 values
+  // save the returned array
+  var array = prompt_values(2);
+  //assign the returned array values from index 0 and 1 into the numbers to use
   number_1 = array[0];
   number_2 = array[1];
-  switch (type) {
+  // switch on the operator
+  switch (operator) {
+    //if operator is 1= +,
     case 1:
-      return alert("The addition of numbers: Number 1:" +number_1 + " + " + "Number 2:"+ number_2+ " = " + (number_1+number_2));
+    // then preform the addition and return the value as an alert
+      return alert("The addition of numbers: " +number_1 + " + " +  number_2+ " = " + (number_1+number_2));
       break;
+    // if operator is 2 = -
     case 2:
-      return alert("The subtraction of numbers: Number 1:" +number_1 + " - " + "Number 2:"+ number_2+ " = "+ (number_1-number_2));
+    // then preform the addition and return the value as an alert
+      return alert("The subtraction of numbers: " +number_1 + " - " + number_2+ " = "+ (number_1-number_2));
       break;
+    // if operator is 3 = *
     case 3:
-      return alert("The mulitplication of numbers: Number 1:" +number_1 + " * " + "Number 2:" + number_2  +" = "+ (number_1*number_2));
+    // then preform the addition and return the value as an alert
+      return alert("The mulitplication of numbers: " +number_1 + " * " + number_2  +" = "+ (number_1*number_2));
       break;
+    // if operator is 4 = /
     case 4:
-      return alert("The division of numbers: Number 1 -:" +number_1 + " / " + "Number 2 -:"+ number_2 +" = "+ (number_1/number_2));
+    // then preform the addition and return the value as an alert
+      return alert("The division of numbers: -:" +number_1 + " / " + number_2 +" = "+ (number_1/number_2));
       break;
     default:
   }
 }
 
 function bmiCalc() {
+  //prompt whether user wants to use the imperial or metric system
   var type =  parseInt(prompt("please chose your operation \n" + "1 - Imperial (lb)\n" +" 2 - Metric (kg)"));
-  var weight;
-  var height;
+  // save the value in our array
+  var array = prompt_values(2,"please enter your weight","please enter your height" )
+  var weight = array[0];
+  var height = array[1];
+  // conditional if the type is imperial
   if (type == 1) {
-    weight = parseFloat(prompt("please enter your weight(lb)"));
-    height = parseFloat(prompt("please enter your height (in)"));
-    return alert("Your BMI is :" + weight / Math.pow(height,2) * 703);
+    // we carry out the imperial formula for BMI calculation
+    return alert("Your BMI is :" + (weight / Math.pow(height,2) * 703).toFixed(2));
+    // conditional if the type is metric
   }else if(type == 2 ){
-    weight = parseFloat(prompt("please enter your weight(KG)"));
-    height = parseFloat(prompt("please enter your height (M)"));
-    return alert("Your BMI is : " +weight / Math.pow(height,2));
+    //we carry our the metric formula
+    return alert("Your BMI is : " + (weight / Math.pow(height,2).toFixed(2)).toFixed(2));
   }
 
 }
+//funtion to calculate the trip
 function tripCalc() {
-   alert("Please prepare the following information\n" + "distance\n"+ "fuel efficiency (mpg)\n" + "cost per gallon\n" + "speed\n");
-  var distance, miles_per_gallon,cost_per_litre,speed;
-  distance = parseFloat(prompt("please enter your distance(miles)"));
-  miles_per_gallon = parseFloat(prompt("please enter your miles per gallon (mpg)"));
-  cost_per_gallon = parseFloat(prompt("please enter your cost per gallon(£/gallon)"));
-  speed = parseFloat(prompt("please enter your speed(mph)"));
-
-  console.log(distance);
-  console.log(miles_per_gallon);
-  console.log(cost_per_litre);
+  //send a message to ask the user to have the values ready
+  alert("Please prepare the following information\n" + "distance\n"+ "fuel efficiency (mpg)\n" + "cost per gallon\n" + "speed\n");
+  // use the fuction to prompt the user to provide the details required
+  var array =  prompt_values(4,"please enter your distance(miles)","please enter your miles per gallon (mpg)","please enter your cost per gallon(£/gallon)","please enter your speed(mph)")
+  // assign the return value of the prompt from the array to each individual variable
+  var distance =array[0];
+  var miles_per_gallon =array[1];
+  var cost_per_gallon =array[2];
+  var speed =array[3];
+  // to account for the loss on miles_per_gallon past 60 mph
+  // for each mph past 60, we lose 2 miles_per_gallon. so modulus 60,returns remainder of 60.
+  // mulitply the return by 2 to get the actual miles_per_gallon
+  // we then divide this new miles_per_gallon by the distance to find the actual gallons needed
+  // then times the return of the final calculation by the cost_per_galon
   var price = (distance/( miles_per_gallon - (speed %60) * 2)) * cost_per_gallon;
+  // to find the duration we just need to divide the distance by mph.
+  // which returns hours. the .toFixed(2) is to trunkate the decimals to 2 only.
   var duration = (distance/speed).toFixed(2);
   alert("Your trip will take "+ duration + "hours and cost £" + price.toFixed(2)) ;
 }
