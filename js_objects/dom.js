8198546f943617d19b28d731549a2cbd53689822
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-08-09 13:53:26 +0100

    added funciton for injecting class to tag

--- js_objects/dom.js
+++ js_objects/dom.js
@@ -1,7 +1,7 @@
 // get element by id name
 var wrapperDiv = document.getElementById("wrapper");
 // style is property of the wrapper
-wrapperDiv.style.backgroundColor = "lightgrey";
+// wrapperDiv.style.backgroundColor = "lightgrey";
 
 // get element by tag name
 var lis = document.getElementsByTagName("li");
@@ -21,8 +21,21 @@ for (var i = 0; i < selected.length; i++) {
 var ptag = document.createElement("p");
 ptag.innerHTML = "a brand new tag";
 document.body.appendChild(ptag);
-
+// get element
 var ul = document.getElementById("list");
+//create new element to add
 var listItem = document.createElement("li");
+// set the value of the new li element
 listItem.innerHTML = "new list item";
+// insert the newly created element at second to last position
 ul.insertBefore(listItem,lis[lis.length - 1]);
+
+// turn every other list item to different color
+for (var i = 0; i < lis.length; i++) {
+  var className = i % 2 == 0 ? "even": "";
+  var li = lis[i];
+
+  li.className = className;
+}
+var countSpan = document.getElementById("count");
+countSpan.innerHTML = lis.length;
